d33929a51d9ac6805fbb5ccddc928cb5195cc865
Abhijit Shaw <abhijeetshaw2003@gmail.com>
2023-01-02 21:28:08 +0530

    leap_year code added

---
+++ for_loop/leap_year.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int year;
+    cout<<"year";
+    cin>>year;
+
+    if(year%4==0){
+        cout<<"leap year";
+    }
+    else{
+      cout<<" not aleap year";   
+    }
+    return 0;
+}
